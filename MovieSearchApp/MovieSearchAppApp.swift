731dcba961953233d26e4ae269f3952e29b20a75
Harsha <harsha.87b@gmail.com>
2024-06-28 11:26:00 -0500

    Initial Commit

---
+++ MovieSearchApp/MovieSearchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieSearchAppApp.swift
+//  MovieSearchApp
+//
+//  Created by BV Harsha on 2024-06-28.
+//
+
+import SwiftUI
+
+@main
+struct MovieSearchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
